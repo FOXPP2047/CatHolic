99daeb40a91f5e06602a400f7f1fd8121939ffd3
Yongmin Cho <qlxhdl55@gmail.com>
2020-11-13 00:17:53 -0800

    Cat Animation

--- client/Assets/Scripts/GameMechanism/catAnimator.cs
+++ client/Assets/Scripts/GameMechanism/catAnimator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class catAnimator : MonoBehaviour
+public class CatAnimator : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody rigidbody;
